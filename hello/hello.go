bc1f46c52ef4a6c762f40288b3b17eb8b3e8b3e8
mukilane <mukilane@gmail.com>
2018-01-02 21:52:42 +0530

    Getting started with Gobot

---
+++ hello/hello.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"github.com/tarm/serial"
+)
+
+func main() {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+			log.Fatal(err)
+	}
+	
+	n, err := s.Write([]byte("test"))
+	if err != nil {
+			log.Fatal(err)
+	}
+	
+	buf := make([]byte, 128)
+	for {
+		n, err = s.Read(buf)
+		if err != nil {
+				log.Fatal(err)
+		}
+		log.Printf("%q", buf[:n])
+	}
+}
